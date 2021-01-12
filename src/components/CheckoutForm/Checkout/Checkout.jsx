53460a1798b6a6ed521e1879103021d407556ab0
NoblePrinceii <farhan.begg@students.makeschool.com>
2021-01-11 17:01:48 -0800

    console.logging token

---
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -0,0 +1,64 @@
+import React,{ useState, useEffect } from 'react'
+import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core'
+import useStyles from './styles'
+import AddressForm from '../AddressForm'
+import PaymentForm from '../PaymentForm'
+import {commerce } from '../../../lib/commerce';
+const steps = ['Shipping address', 'Payment details ']
+
+
+const Checkout = () => {
+    const [activeStep, setActiveStep] = useState(0)
+    const [checkoutToken, setCheckoutToken] = useState(null)
+    const classes = useStyles();
+    
+    useEffect(() => {
+        const generateToken = async () => {
+            try {
+                const token = await commerce.checkout.generateToken(cart.id, {type: 'cart'})
+                console.log(token)
+                setCheckoutToken(token)
+            } catch (error) {
+
+            }
+        }
+
+        generateToken();
+
+    }, [])
+
+    const Confirmation = () => (
+        <div>
+            Confirmation
+        </div>
+    )
+   
+   
+    // if the form is on step 0 display address form if not payment form
+    const Form = () => ( activeStep === 0
+        ? <AddressForm />
+        : <PaymentForm />
+    )
+    return (
+        <>
+            <div className={classes.toolbar} />
+            <main  className={classes.layout} >
+                <Paper className={classes.paper}>
+                    <Typography cariant="h4" align="center">Checkout</Typography>
+                    <Stepper activeStep={activeStep} className={classes.stepper}>
+                        {steps.map((step) => (
+                            <Step key={step}>
+                                <StepLabel>{step}</StepLabel>
+                            </Step>
+                        ))}
+                        </Stepper>
+                
+                        {activeStep === steps.length ? <Confirmation /> : <Form/>}
+                </Paper>
+            </main>
+        </>
+      
+    )
+}
+
+export default Checkout
